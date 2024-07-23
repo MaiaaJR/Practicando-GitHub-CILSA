bec437db26ff89b0c25cf2d531f1c2942e50b3a4
Maia <maiaros.17@gmail.com>
2024-07-23 20:41:31 -0300

    corrigiendo el orden y código de las validaciones

--- Mi formulario con Bootstrap/script.js
+++ Mi formulario con Bootstrap/script.js
@@ -5,26 +5,35 @@ document.addEventListener('DOMContentLoaded', (event) => {
         event.preventDefault();
         event.stopPropagation();
 
-        if (formulario.checkValidity() === false) {
-            formulario.classList.add('was-validated');
-            return;
-        }
-
         var nombre = document.getElementById('nombre').value;
         var apellido = document.getElementById('apellido').value;
         var direccionEmail = document.getElementById('direccionEmail').value;
 
-        if (!/^[a-zA-Z ]+$/.test(nombre) || !/^[a-zA-Z ]+$/.test(apellido)) {
-            alert('Ingresar solo letras y espacios en el caso de agregar más de un nombre');
-            return;
+        if (!/^[a-zA-Z ]+$/.test(nombre)) {
+            alert('Ingresar solo letras y espacios en el campo de nombre.');
+            isValid = false;
+        }
+
+        if (!/^[a-zA-Z ]+$/.test(apellido)) {
+            alert('Ingresar solo letras y espacios en el campo de apellido.');
+            isValid = false;
         }
 
         if (!/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/.test(direccionEmail)) {
-            alert('Formato no válido. Ejemplo de formato válido: juan@mmm.com');
-            return;
+            alert('Formato de correo no válido. Ejemplo de formato válido: juan@mmm.com');
+            isValid = false;
         }
 
-        formulario.submit();
+        if (isValid) {
+            if (formulario.checkValidity() === false) {
+                formulario.classList.add('was-validated');
+                return;
+            }
+
+            formulario.submit();
+        } else {
+            formulario.classList.add('was-validated');
+        }
     });
 
     document.getElementById('fondoClaro').addEventListener('click', function() {
